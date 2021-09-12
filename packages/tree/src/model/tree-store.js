c517e203ec7a46698e3221aa326d7ec8d5d1048d
chi <chirm7@163.com>
2021-09-12 23:02:20 +0800

    初始化tree组件：tree-store

---
+++ packages/tree/src/model/tree-store.js
@@ -0,0 +1,8 @@
+export default class TreeStore {
+  constructor(options) {
+    this.currentNode = null
+    this.childNodes = []
+    this.root = ''
+    console.log(options)
+  }
+}
--- packages/tree/src/tree.vue
+++ packages/tree/src/tree.vue
@@ -1,13 +1,69 @@
 <template>
-  <div>this is tree</div>
+  <div class="rm-tree">
+    <rm-tree-node></rm-tree-node>
+    <div class="el-tree__empty-block"
+         v-if="!root.childNodes || root.childNodes.length === 0">
+      <span class="el-tree__empty-text">{{ emptyText }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
+import RmTreeNode from './tree-node.vue'
+import TreeStore from './model/tree-store'
+
 export default {
-  name: 'RmTree'
+  name: 'RmTree',
+  components: {
+    RmTreeNode
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    },
+    emptyText: {
+      type: String,
+      default: '暂无数据'
+    }
+  },
+  data() {
+    return {
+      root: null,
+      store: null,
+    }
+  },
+  methods: {
+
+  },
+  created() {
+    this.store = new TreeStore({
+      data: this.data
+    })
+
+    this.root = this.store.root
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.rm-tree {
+  cursor: default;
+  background: #fff;
+  border: 1px solid #d1dbe5;
+}
+.el-tree__empty-block {
+  position: relative;
+  min-height: 60px;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  .el-tree__empty-text {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    color: #5e7382;
+  }
+}
 </style>
